2a1004ce2e72a7f2bf4b1c2dde865f014491f6e4
Kaisma Penn-Titley <kptitley@gmail.com>
2017-07-18 22:44:44 -0400

    modified divisor to 5 instead of 3

--- printcount.js
+++ printcount.js
@@ -1,9 +1,9 @@
 //Printing Numbers 1-100 & Print Match When Divisible by 3
-var divisor = 3;
+var divisor = 5;
 
 for (counter = 1; counter <= 100; counter++) {
-    if (counter % divisor == 0) {
-            console.log("MATCH"!);
+    if (counter % divisor ==0) {
+            console.log("MATCH");
     } else {
         console.log(counter);
     }
