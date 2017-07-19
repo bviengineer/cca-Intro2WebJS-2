498de5bd3a15b0ecf630f11fe7ad9bf36c0f92f7
Kaisma Penn-Titley <kptitley@gmail.com>
2017-07-18 21:56:20 -0400

    prints up to 100 & match at intervals of 3

---
+++ printcount.js
@@ -0,0 +1,9 @@
+//Printing Numbers 1-100 & Print Match When Divisible by 3
+
+for (counter = 1; counter <= 100; counter++) {
+    if (counter % 3 ==0) {
+            console.log("MATCH");
+    } else {
+        console.log(counter);
+    }
+}
